d28289789d4ac1783b3a55b387091b5c371e3f82
Øyvind Knustad <oyk25@hotmail.com>
2023-05-01 22:33:17 +0200

    tying without dotenv in server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+
 
 // Import express into the app variable and import expressLayouts
 const express = require('express')
